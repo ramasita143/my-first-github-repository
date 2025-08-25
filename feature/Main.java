be5950e7e60ccbfb938465a39eb7b74d072ee5d2
chavadhanush <dhanushchava2023@gmail.com>
2025-08-25 13:26:28 +0530

    Modified Main.java locally

--- feature/Main.java
+++ feature/Main.java
@@ -1,6 +1,7 @@
-public class Main{
-public static void main(String[] args){
-  System.out.println("Hii")
-}
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hii");
+  }
 
 }
+// This file was modified locally.
